03b335b89e2351c01ae5a9ab8eac30fbcbf02b50
舘野元 <ev30071@GENs-MacBook-Air.local>
2018-04-22 02:06:54 +0900

    add new file

---
+++ test01.c
@@ -0,0 +1,21 @@
+//struct01.c
+#include <stdio.h>
+#include <string.h>
+
+struct Man {
+  char name[32];
+  int age;
+};
+
+int main()
+{
+  struct Man student;
+  
+  strcpy(student.name, "山田太郎");
+  student.age = 16;
+  
+  printf("生徒の名前は%sです\n", student.name);
+  printf("歳は%d歳です\n", student.age);
+  
+  return 0;
+}
